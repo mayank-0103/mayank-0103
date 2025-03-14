dacfbc1229ebf97ebebd09a6e09ccba39f76c714
mayank-0103 <201279422+mayank-0103@users.noreply.github.com>
2025-03-14 16:28:19 +0530

    Created landing page

---
+++ script.js
@@ -0,0 +1,19 @@
+// Getting elements
+const elem_1 = document.querySelector('#card1');
+const elem_2 = document.querySelector('#card2');
+const elem_3 = document.querySelector('#card3');
+const elem_4 = document.querySelector('#card4');
+
+// Adding links to each of them
+elem_1.addEventListener("click", function () {
+    window.location.href = "https://mayank-0103.github.io/Calculator-M/";
+})
+elem_2.addEventListener("click", function () {
+    window.location.href = "https://mayank-0103.github.io/Image-Gallery/";
+})
+elem_3.addEventListener("click", function () {
+    window.location.href = "https://mayank-0103.github.io/Guess-the-number/";
+})
+elem_4.addEventListener("click", function () {
+    window.location.href = "https://github.com/mayank-0103";
+})
