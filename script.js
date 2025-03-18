429b568772f99ca093890dae81945186241f7a54
mayank-0103 <201279422+mayank-0103@users.noreply.github.com>
2025-03-18 17:08:56 +0530

    Bus fixes

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const elem_2 = document.querySelector('#card2');
 const elem_3 = document.querySelector('#card3');
 const elem_4 = document.querySelector('#card4');
 
+function body_width() {
+    document.body.style.width = (95/100*document.documentElement.clientWidth) + "px";
+};
+
+body_width();
+window.onresize = body_width;
+
 // Adding links to each of them
 elem_1.addEventListener("click", function () {
     window.location.href = "https://mayank-0103.github.io/Calculator-M/";
